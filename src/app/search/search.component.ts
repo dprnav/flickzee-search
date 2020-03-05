1b8793a93bd4e2cacbced70292cfeb042f705d9c
Pranav Dixit <dprnav@gmail.com>
2020-03-05 15:09:35 +0530

    main done

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -55,16 +55,22 @@ export class SearchComponent implements OnInit {
   }
 
   autocomplete(inp) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
     var currentFocus;
     var self = this;
+    /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function(e) {
         var a, b, i, val = this.value;
-        closeAllLists();
+        /*close any already open lists of autocompleted values*/
+        closeAllLists(null);
         if (!val) { return false;}
         currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
         a = document.createElement("DIV");
         a.setAttribute("id", this.id + "autocomplete-list");
         a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         self._searchService.searchMovies(val).subscribe(response =>
         {
@@ -76,42 +82,54 @@ export class SearchComponent implements OnInit {
             b.innerHTML += "<input type='hidden' value='" + item.MovieName + "'>";
             b.addEventListener("click", function(e) {
               inp.value = this.getElementsByTagName("input")[0].value;
-              closeAllLists();
+              closeAllLists(null);
             });
             a.appendChild(b);
           });
         });
-
     });
+    /*execute a function presses a key on the keyboard:*/
     inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
+        var y = <HTMLDivElement>document.getElementById(this.id + "autocomplete-list");
+        if (y) var x = y.getElementsByTagName("div");
         if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
           currentFocus++;
+          /*and and make the current item more visible:*/
           addActive(x);
         } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
           currentFocus--;
+          /*and and make the current item more visible:*/
           addActive(x);
         } else if (e.keyCode == 13) {
-          e.preventDefault();
           if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
             if (x) x[currentFocus].click();
           }
         }
     });
     function addActive(x) {
+      /*a function to classify an item as "active":*/
       if (!x) return false;
+      /*start by removing the "active" class on all items:*/
       removeActive(x);
       if (currentFocus >= x.length) currentFocus = 0;
       if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
       x[currentFocus].classList.add("autocomplete-active");
     }
     function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
       for (var i = 0; i < x.length; i++) {
         x[i].classList.remove("autocomplete-active");
       }
     }
     function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
       var x = document.getElementsByClassName("autocomplete-items");
       for (var i = 0; i < x.length; i++) {
         if (elmnt != x[i] && elmnt != inp) {
@@ -119,6 +137,7 @@ export class SearchComponent implements OnInit {
       }
     }
   }
+  /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
       closeAllLists(e.target);
   });
@@ -130,8 +149,7 @@ export class SearchComponent implements OnInit {
         this.searchQuery = params['query'];
         this.search(params['query']);
       });
-      console.log("hello");
-      this.autocomplete(document.getElementById("searchTerm"));
+    this.autocomplete(document.getElementById("searchTerm"));
   }
 
 }
